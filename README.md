6d3753bd8de9f3d66a21b0169d993f55a963f978
Mustafo Yakubov <mustafa6143170@gmail.com>
2022-08-21 01:20:31 +0500

    push

--- README.md
+++
@@ -1,1 +0,0 @@
-## Video tutorial: https://www.youtube.com/watch?v=SugvoBXf2bU
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import { AuthUserProvider } from '../context/AuthUserContext'
-
-function MyApp({ Component, pageProps }) {
-  return <AuthUserProvider><Component {...pageProps} /></AuthUserProvider>
-}
-
-export default MyApp;
