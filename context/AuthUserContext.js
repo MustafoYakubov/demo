175496ac680841960fa681d3d1548ca77bbe7a2b
Mustafo Yakubov <mustafa6143170@gmail.com>
2022-08-21 01:53:06 +0500

    push done inshallah

--- context/AuthUserContext.js
+++ context/AuthUserContext.js
@@ -14,4 +14,4 @@ export function AuthUserProvider({children}) {
     return <authUserContext.Provider value={auth}>{children}</authUserContext.Provider>
 }
 
-export const useAuth = () => useContext(authUserContext);
+export const UseAuth = () => useContext(authUserContext);
